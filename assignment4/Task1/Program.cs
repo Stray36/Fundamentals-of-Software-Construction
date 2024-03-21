d9159d2d919529262d4de6ef832d56f3bcad7813
unknown <yyxjkfz2022@gmail.com>
2024-03-21 16:50:38 +0800

    add assignment4

---
+++ assignment4/Task1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t) 
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+    
+        public void Foreach(Action<T> action)
+        {
+            Node<T> n = head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            GenericList<int> list = new GenericList<int>();
+            // 初始化链表元素
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            // 1. 打印输出
+            list.Foreach(m => Console.WriteLine(m));
+            // 2. 求最小值
+            int min = 100;
+            Action<int> action1 = delegate (int x)
+            {
+                if (x < min)
+                {
+                    min = x;
+                }
+            };
+            list.Foreach(action1);
+            Console.WriteLine($"最小值是：{min}");
+            // 3. 求最大值
+            int max = -1;
+            Action<int> action2 = delegate (int x)
+            {
+                if (x > max)
+                {
+                    max = x;
+                }
+            };
+            list.Foreach(action2);
+            Console.WriteLine($"最大值是：{max}");
+            // 4.求和
+            int sum = 0;
+            Action<int> action3 = delegate (int x)
+            {
+                sum += x;
+            };
+            list.Foreach(action3);
+            Console.WriteLine($"元素之和为：{sum}");
+        }
+    }
+}
---
+++ assignment4/Task2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading;
+
+namespace Task2
+{
+    public delegate void ClockSimulator(object sender, ClockEventArgs args);
+
+    public class ClockEventArgs
+    {
+        public int hour {  get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+
+        public bool IsEqual(ClockEventArgs args)
+        {
+            return hour == args.hour && minute == args.minute && second == args.second;
+        }
+        public void PassOneSecond()
+        {
+            second++;
+            if (second == 60)
+            {
+                minute++;
+                second = 0;
+            }
+            if (minute == 60)
+            {
+                hour++;
+                minute = 0;
+            }
+            if (hour == 24)
+            {
+                hour = 0;
+            }
+        }
+    }
+    
+    public class ClockPublisher
+    {
+
+        public event ClockSimulator Tick;
+        public event ClockSimulator Alarm;
+
+        public void start(ClockEventArgs tickArgs, ClockEventArgs alarmArgs, int tickSeconds)
+        {
+            for (int i = 0; i < tickSeconds; i++)
+            {
+                ClockTick(tickArgs, tickSeconds);
+                if (tickArgs.IsEqual(alarmArgs))
+                {
+                    ClockAlarm(alarmArgs);
+                }
+                System.Threading.Thread.Sleep(1000);
+                tickArgs.PassOneSecond();
+            }
+        }
+
+        public void ClockTick(ClockEventArgs args, int tickSeconds)
+        {
+            Tick(this, args);
+        }
+
+        public void ClockAlarm(ClockEventArgs args)
+        {
+            Alarm(this, args);
+        }
+
+    }
+
+
+    public class ClockSubscriber
+    {
+        public ClockPublisher clock = new ClockPublisher();
+
+        public ClockSubscriber() 
+        {
+            clock.Tick += Sub_Tick;
+            clock.Alarm += Sub_Alarm;
+        }
+
+        void Sub_Tick(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine($"Tick.. {args.hour}:{args.minute}:{args.second}");
+        }
+
+        void Sub_Alarm(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine($"Alarm! {args.hour}:{args.minute}:{args.second}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockSubscriber clock1 = new ClockSubscriber();
+            ClockEventArgs tickArgs = new ClockEventArgs()
+            {
+                hour = 16,
+                minute = 24,
+                second = 36
+            };
+            ClockEventArgs alarmArgs = new ClockEventArgs()
+            {
+                hour = 16,
+                minute = 24,
+                second = 40
+            };
+            clock1.clock.start(tickArgs, alarmArgs, 10);
+        }
+    }
+}
